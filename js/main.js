f046d1daf3f7b4b776381ac3cde8b4ddf3df919d
dastonrajapboyev <dastonrajapboyev79@gmail.com>
2024-07-19 00:27:50 +0500

    90% finished

--- js/main.js
+++ js/main.js
@@ -28,3 +28,23 @@ window.addEventListener("click", function (event) {
     modal.style.display = "none";
   }
 });
+//
+//==================================
+
+let profileModal = document.querySelector(".profile-info_modal");
+let user = document.querySelector(".user-prof");
+
+user.addEventListener("click", function (event) {
+  profileModal.classList.add("show-modal");
+  event.stopPropagation();
+});
+
+window.addEventListener("click", function (event) {
+  if (!profileModal.contains(event.target)) {
+    profileModal.classList.remove("show-modal");
+  }
+});
+
+profileModal.addEventListener("click", function (event) {
+  event.stopPropagation();
+});
--- js/profile-modal.js
+++ js/profile-modal.js
@@ -1,3 +1,23 @@
+// let modal = document.getElementById("myModal");
+
+// let btn = document.querySelector(".add-plus");
+
+// let closeBtn = document.querySelector(".close");
+
+// btn.addEventListener("click", function () {
+//   modal.style.display = "flex";
+// });
+
+// closeBtn.addEventListener("click", function () {
+//   modal.style.display = "none";
+// });
+
+// window.addEventListener("click", function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// });
+
 const burgerMenu = document.querySelector(".burger-menu");
 const flightInfo = document.querySelector(".flight-info");
 const rightColumn = document.querySelector(".right-column");
